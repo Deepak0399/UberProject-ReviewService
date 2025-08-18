d16a2d381add9e03a8d55a5bbe189f3c4737ecdb
Deepak <deepakkumar97967@gmail.com>
2025-08-18 22:29:25 +0530

    Added Review Repository using JpaRepository

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,28 +1,38 @@
 package com.example.uberreviewservice.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "bookingreview")
 public class Review {
+
     @Id
-    long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     @Column(nullable = false)
-    String content;
+    private String content;
 
-    Double rating;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    Date createdAt;
+    private Double rating;
 
     @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    Date updatedAt;
+    @Temporal(TemporalType.TIMESTAMP)// this annotation tells spring to manage the format of date in database
+    @CreatedDate// this annotation tells spring to only handle it for object creation
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)// this annotation tells spring to manage the format of date in database
+    @LastModifiedDate// this annotation tells spring to only handle it for object update
+    private Date updatedAt;
 }
