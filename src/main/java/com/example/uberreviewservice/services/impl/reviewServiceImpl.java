53e765d64f8d5ae86f4d41212c5623218790b1fa
Deepak <deepakkumar97967@gmail.com>
2026-01-17 22:58:25 +0530

    Added Review Service

---
+++ src/main/java/com/example/uberreviewservice/services/impl/reviewServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.uberreviewservice.services.impl;
+
+public class reviewServiceImpl {
+}
---
+++ src/main/java/com/example/uberreviewservice/services/interfaces/ReviewService.java
@@ -0,0 +1,22 @@
+package com.example.uberreviewservice.services.interfaces;
+
+import com.example.uberreviewservice.models.Review;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface ReviewService {
+
+    Optional<Review> findReviewById(Long id);
+
+    List<Review> findAllReviews();
+
+    boolean deleteReviewById();
+
+    Review createReview(Review review);
+
+    Review updateReview(Long id, Review review);
+
+}
